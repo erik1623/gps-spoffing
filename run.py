b63a2d7faed2837f96b3ffde4455a0fe656727c5
uri <rotshtein@gmail.com>
2017-05-14 14:11:30 +0300

    Get directories location from environment

--- run.py
+++ run.py
@@ -7,7 +7,10 @@ import argparse
 # import zlib
 import subprocess
 
+
+
 def get_ephemeris(ephemeris_directory):
+	global GZIP_DIR
 	yday = time.localtime().tm_yday
 	eFile = ephemeris_directory + '/brdc' + str(yday) + '0.17n'
 	
@@ -19,7 +22,7 @@ def get_ephemeris(ephemeris_directory):
 		else:
 			dFile = eFile + '.Z'
 		print 'Uncompress...\n\r'
-		subprocess.call('"c:\\Program Files (x86)\\7-Zip\\7z" e '+ dFile + ' -o' + ephemeris_directory, shell=True)
+		subprocess.call(GZIP_DIR + '7z" e '+ dFile + ' -o' + ephemeris_directory, shell=True)
 		print 'Finish to Uncompress\n\r'
 	return eFile
 
@@ -35,25 +38,49 @@ def buildIQ(eFile, duration, csv_file):
 
 	
 def start_broadcast(binFile):
+	global HACKRF_DIR 
+	print 'HACKRF_DIR = ' + HACKRF_DIR
 	print 'hackrf_transfer from ' + binFile + '\n\r'
-	subprocess.call(['../Hackrf/hackrf_transfer', '-t',  binFile,'-f', '1575420000', '-s', '4000000', '-a', '1', '-x', '1'])
-		
+	#subprocess.call([HACKRF_DIR + 'hackrf_transfer', '-t',  binFile,'-f', '1575420000', '-s', '4000000', '-a', '1', '-x', '1', '-R'],shell=True)
+	subprocess.call(HACKRF_DIR + 'hackrf_transfer -t ' + binFile + ' -f 1575420000 -s 4000000 -a 1 -x 1 -R',shell=True)
 def help():
 	s = 'Environment settings\n'
 	s += '\tEphemeris directory stored by default at ./Files. Can by set by EPHEREMIS_DIR\n'
-	s += '\thackrf_transfer.exe location is set by default to ../Hackrf/. Can by set by HACKRF_DIR\n'
-	s += '\t7z.exe location is set by default to "c:\\Program Files (x86)\\7-Zip\\7z". Can by set by 7Z_DIR\n'
+	s += '\thackrf_transfer.exe location is set by default to "C:/Program Files/GNURadio-3.7/bin/". Can by set by HACKRF_DIR\n'
+	s += '\t7z.exe location is set by default to "c:\\Program Files (x86)\\7-Zip\\7z". Can by set by GZIP_DIR\n'
 	return s
 	
+def update_dirs():
+	global FILES_DIR
+	global HACKRF_DIR 
+	global GZIP_DIR
+	FILES_DIR = os.environ.get('FILES_DIR')
+	HACKRF_DIR = os.environ.get('HACKRF_DIR')
+	GZIP_DIR = os.environ.get('GZIP_DIR')
+	
+	if FILES_DIR is None:
+		FILES_DIR = './Files'
+
+	if HACKRF_DIR is None:
+		HACKRF_DIR = '"C:/Program Files/GNURadio-3.7/bin/"'
+
+	if GZIP_DIR is None:
+		GZIP_DIR = 'c:\\Program Files (x86)\\7-Zip\\'
+	
 def main():
+	global FILES_DIR
+	global HACKRF_DIR 
+
+	update_dirs()
 	csv_file = None
 	parser = argparse.ArgumentParser(description='Run.py')
 	parser._optionals.title = help()
 	parser.add_argument('-d', action="store", dest='duration', type=str, help='Running suration in seconds', default='300')
 	parser.add_argument('-u', action="store", dest='csv_file', type=str, help='Set route csv file', default=None)
 	results = parser.parse_args()
-	print results
-	ephemerisFile = get_ephemeris('./Files')
+
+	ephemerisFile = get_ephemeris(FILES_DIR)
+	print 'ephemerisFile = ' + ephemerisFile
 	bin_file = buildIQ(ephemerisFile, results.duration, results.csv_file)
 	start_broadcast(bin_file)
 
